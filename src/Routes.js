d9ca9c5ff34b021aa332adb8c1d88de6d07c992d
Bryan <Jbrewton88@gmail.com>
2019-05-28 17:05:27 -0500

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from "./components/Home/Home"
+import About from "./components/About/About"
+import {Route, Switch} from 'react-router-dom'
+import ClassList from './components/ClassList/ClassList'
+import Student from './components/Student/Student'
+
+export default <Switch>
+    <Route component={Home} exact path="/"/>
+    <Route component={About} path="/about"/>
+    <Route component={ClassList} path='/classlist/:class' />
+    <Route component={Student} path='/student/:id'/>
+</Switch>
